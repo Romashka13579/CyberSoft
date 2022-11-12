69f8ab7be32b28079a89a804c966b035a12985f5
Romashka <rroma13579@gmail.com>
2022-11-12 12:30:59 +0100

    order bar

--- js/order.js
+++ js/order.js
@@ -27,7 +27,7 @@ var actualBlocks = document.querySelectorAll('.actual-slider-main-block');
 
 actualFull.addEventListener('transitionend', () => {
     var actualBlocks = document.querySelectorAll('.actual-slider-main-block');
-    if (actualBlocks[i+2].id === "actual-third-clone") {
+    if (actualBlocks[i + 2].id === "actual-third-clone") {
         actualFull.style.transition = "none";
         i = 3;
         HtmlCssProjectLeft(actualFull);
@@ -54,7 +54,7 @@ actualBtnRight.addEventListener('click', () => {
     actualFull.style.transition = "1s";
 });
 
-function HtmlCssProjectLeft(obj){
+function HtmlCssProjectLeft(obj) {
     obj.style.left = "" + (-390 * i) + "px";
 }
 
@@ -63,4 +63,76 @@ var orderbarBlocksType = document.querySelectorAll('.orderbar-block-type');
 
 orderbarBlocksType.forEach(orderbarBlockType => {
     orderbarBlockType.setAttribute('size', orderbarBlockType.getAttribute('placeholder').length);
+});
+
+
+
+var orderbarBlockDropdown = document.querySelector('.orderbar-block-dropdown');
+var orderbarDropdownMenu = document.querySelector('.orderbar-dropdown-menu');
+var orderbarDropdownTxt = document.querySelector('.orderbar-block-dropdown-txt')
+
+orderbarBlockDropdown.addEventListener('focus', () => {
+    orderbarDropdownMenu.style.display = "flex";
+    setTimeout(() => {
+        orderbarDropdownMenu.style.opacity = 1;
+    }, 10);
+});
+
+orderbarBlockDropdown.addEventListener('focusout', () => {
+    orderbarDropdownMenu.style.opacity = 0;
+});
+
+orderbarDropdownMenu.addEventListener('transitionend', () => {
+    if (orderbarDropdownMenu.style.opacity == 0) {
+        orderbarDropdownMenu.style.display = "none";
+    }
+});
+
+var orderbarDropdownMenuItems = document.querySelectorAll('.orderbar-dropdown-menu-item');
+
+orderbarDropdownMenuItems.forEach(orderbarDropdownMenuItem => {
+    orderbarDropdownMenuItem.addEventListener('click', () => {
+        orderbarDropdownTxt.innerHTML = orderbarDropdownMenuItem.innerHTML;
+    });
+});
+
+var orderbarBlockDropdown2 = document.querySelector('.orderbar-block-dropdown-2');
+var orderbarDropdownMenu2 = document.querySelector('.orderbar-dropdown-menu-2');
+var orderbarDropdownTxt2 = document.querySelector('.orderbar-block-dropdown-txt-2');
+
+orderbarBlockDropdown2.addEventListener('focus', () => {
+    orderbarDropdownMenu2.style.display = "flex";
+    setTimeout(() => {
+        orderbarDropdownMenu2.style.opacity = 1;
+    }, 10);
+});
+
+orderbarBlockDropdown2.addEventListener('focusout', () => {
+    orderbarDropdownMenu2.style.opacity = 0;
+});
+
+orderbarDropdownMenu2.addEventListener('transitionend', () => {
+    if (orderbarDropdownMenu2.style.opacity == 0) {
+        orderbarDropdownMenu2.style.display = "none";
+    }
+});
+
+var orderbarDropdownMenuItems2 = document.querySelectorAll('.orderbar-dropdown-menu-item-2');
+
+orderbarDropdownMenuItems2.forEach(orderbarDropdownMenuItem2 => {
+    orderbarDropdownMenuItem2.addEventListener('click', () => {
+        orderbarDropdownTxt2.innerHTML = orderbarDropdownMenuItem2.innerHTML;
+    });
+});
+
+var orderbarBtn = document.querySelector('.orderbar-btn');
+var orderbarBlockType = document.querySelector('.orderbar-block-type');
+var orderbarBlockType2 = document.querySelector('.orderbar-block-type-2');
+
+orderbarBtn.addEventListener('click', () => {
+    console.log(orderbarDropdownTxt.innerHTML, orderbarDropdownTxt2.innerHTML, orderbarBlockType.value, orderbarBlockType2.value);
+    orderbarDropdownTxt.innerHTML = "Тип проектy";
+    orderbarDropdownTxt2.innerHTML = "Ціна проекту";
+    orderbarDropdownTxt.value = "";
+    orderbarDropdownTxt2.value = "";
 });
