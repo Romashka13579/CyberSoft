5466d964b7c00d0b5ee09f8d69a7f35f83a89b84
Romashka <rroma13579@gmail.com>
2022-12-01 14:15:32 +0100

    added custom properties for theme change

--- js/forall.js
+++ js/forall.js
@@ -16,68 +16,6 @@ navbarRigthBurgerMenu.addEventListener('click', () => {
     }
 });
 
-// var themeInside = document.querySelector('.theme-inside');
-// var languageInside = document.querySelector('.language-inside');
-// var themeBlockOption = document.querySelector('.theme-block-option');
-// var languageBlockOptions = document.querySelectorAll('.language-block-option');
-
-// themeInside.addEventListener('mouseover', () => {
-//     document.querySelector('.theme-block-switched').style.borderRadius = "0px";
-//     themeBlockOption.style.display = "flex";
-//     setTimeout(() => {
-//         themeBlockOption.style.opacity = 1;
-//     }, 10);
-// });
-// themeInside.addEventListener('mouseout', () => {
-//     document.querySelector('.theme-block-switched').style.borderTopLeftRadius = "5px";
-//     themeBlockOption.style.opacity = 0;
-// });
-// themeBlockOption.addEventListener('transitionend', () => {
-//     if (themeBlockOption.style.opacity == 0) {
-//         themeBlockOption.style.display = "none";
-//     }
-// });
-
-// var islanguage = false;
-// var d = 0;
-
-// languageInside.addEventListener('mouseover', () => {
-//     document.querySelector('.language-block-switched').style.borderRadius = "0px";
-//     islanguage = true;
-//     BlockOptions();
-// });
-// languageInside.addEventListener('mouseout', () => {
-//     document.querySelector('.language-block-switched').style.borderBottomLeftRadius = "5px";
-//     islanguage = false;
-//     BlockOptions();
-// });
-// function BlockOptions() {
-//     if (islanguage == true) {
-//         languageBlockOptions[d].style.display = "flex";
-//         setTimeout(() => {
-//             languageBlockOptions[d].style.opacity = 1;
-//         }, 10);
-//     }
-//     else if (islanguage == false) {
-//         languageBlockOptions[d].style.opacity = 0;
-//     }
-// }
-// languageBlockOptions.forEach(languageBlockOption => {
-//     languageBlockOption.addEventListener('transitionend', () => {
-//         if (languageBlockOption.style.opacity == 0) {
-//             languageBlockOption.style.display = "none";
-//         }
-//         if(islanguage == true && d < languageBlockOptions.length-1){
-//             d++;
-//             BlockOptions();
-//         }
-//         else if(islanguage == false && d > 0){
-//             d--;
-//             BlockOptions();
-//         }
-//     });
-// });
-
 var flagsArray = ["POL.webp", "ENG.svg", "DTH.svg", "FRN.webp", "SWD.webp"];
 var languageBlockSwitchedFlags = document.querySelectorAll('.language-block-switched-flag');
 
@@ -110,6 +48,7 @@ var themeBlockOption = document.querySelector('.theme-block-option');
 var themeBlockSwitched = document.querySelector('.theme-block-switched');
 
 themeBlockOption.addEventListener('click', () => {
+    document.documentElement.classList.toggle("root-dark");
     var switchedTxt = themeBlockSwitched.querySelector('.theme-block-switched-txt');
     var switchedTxtClone = switchedTxt.cloneNode(true);
     var optionTxt = themeBlockOption.querySelector('.theme-block-switched-txt');
